a559a37420582fbab028954491ceda4c0b7359a3
LFDM <1986gh@gmail.com>
2017-03-08 09:08:47 +0100

    feat(contact-list) Add some styling

--- src/components/ContactList/index.js
+++ src/components/ContactList/index.js
@@ -1,24 +1,34 @@
 import { flow, map } from 'lodash';
-import { Card } from 'components/Layout';
+import { Page, Card } from 'components/Layout';
+import Avatar from 'components/ui/Avatar';
 import withResolve from 'hocs/withResolve';
 import withOwnerId from 'hocs/withOwnerId';
+
+import styles from './styles.scss';
 
 import api from 'api';
 
 function ContactList({ contacts }) {
   return (
-    <div>
-      { map(contacts, (contact) => (
-        <Contact key={ contact.id } contact={ contact } />
-      ))}
-   </div>
+    <Page>
+      <h2>Contact List</h2>
+      <div>
+          { map(contacts, (contact) => (
+            <Contact key={ contact.id } contact={ contact } />
+          ))}
+      </div>
+   </Page>
  );
 }
 
 function Contact({ contact }) {
   return (
-    <Card>
-       { contact.name }
+    <Card className={ styles.contact }>
+      <Avatar src={ contact.avatar } size="50" />
+      <div className={ styles.details }>
+        <div className={ styles.name }>{ contact.name }</div>
+        <div className={ styles.email }>{ contact.email }</div>
+      </div>
     </Card>
   );
 }
