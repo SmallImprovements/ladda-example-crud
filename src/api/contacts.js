ca1204dc3e14598f249cebb13e7d0c588d81e63a
LFDM <1986gh@gmail.com>
2017-03-09 08:08:08 +0100

    refactor(contacts-api) Rename

--- src/api/contacts.js
+++ src/api/contacts.js
@@ -2,31 +2,31 @@ import axios from 'axios';
 import { ROOT } from 'constants/paths';
 
 const BASE_URL = `${ROOT}/api/contacts`;
-const extractData = (response) => response.data;
+const getData = (response) => response.data;
 
 getContacts.operation = 'READ';
 export function getContacts(ownerId) {
-  return axios.get(BASE_URL, { params: { ownerId } }).then(extractData);
+  return axios.get(BASE_URL, { params: { ownerId } }).then(getData);
 }
 
 getContact.operation = 'READ';
 getContact.byId = true;
 export function getContact(id) {
-  return axios.get(`${BASE_URL}/${id}`).then(extractData);
+  return axios.get(`${BASE_URL}/${id}`).then(getData);
 }
 
 updateContact.operation = 'UPDATE';
 export function updateContact(contact) {
-  return axios.patch(`${BASE_URL}/${contact.id}`, contact).then(extractData);
+  return axios.patch(`${BASE_URL}/${contact.id}`, contact).then(getData);
 }
 
 createContact.operation = 'CREATE';
 createContact.invalidates = ['getContacts'];
 export function createContact(contact) {
-  return axios.post(BASE_URL, contact).then(extractData);
+  return axios.post(BASE_URL, contact).then(getData);
 }
 
 deleteContact.operation = 'DELETE';
 export function deleteContact(id) {
-  return axios.delete(`${BASE_URL}/${id}`).then(extractData);
+  return axios.delete(`${BASE_URL}/${id}`).then(getData);
 }
